46b6fc9c6ca69f1358ab5ecb32f5b82e0305c9b5
pumaatlarge <fangmagic@gmail.com>
2015-02-10 18:39:12 -0700

    Q2_19

---
+++ src/chapter2/Q2_19.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+/**
+ * Created by springtree on 2/10/2015.
+ */
+public class Q2_19 {
+    // my own answer, but it doesn't work when SIZE = 3
+    public static final int SIZE = 3;
+    public static void main(String[] args) {
+        /*for (int i = 0; i < SIZE * SIZE; i++)   {
+            for (int j = 0; j < SIZE * SIZE ; j++) {
+                if (i % (SIZE + 1) == 0 && j % (SIZE + 1) == 0)   {
+                    System.out.print("+");
+                }
+                else if (i % (SIZE + 1) == 0)    {
+                    System.out.print("=");
+                }
+                else if (j % (SIZE + 1) == 0) {
+                    System.out.print("|");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();*/
+        }
+    }
+}
