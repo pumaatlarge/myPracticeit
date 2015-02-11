dd48318c7fdb0eaf31da02e63100532abdd1145e
pumaAtLarge <fangmagic@gmail.com>
2015-02-10 22:19:57 -0700

    Q2.19 correction

--- src/chapter2/Q2_19.java
+++ src/chapter2/Q2_19.java
@@ -4,8 +4,8 @@ package chapter2;
  * Created by springtree on 2/10/2015.
  */
 public class Q2_19 {
-    // my own answer, but it doesn't work when SIZE = 3
-    public static final int SIZE = 3;
+    // my own answer, but it doesn't work when SIZE = 1
+    public static final int SIZE = 1;
     public static void main(String[] args) {
         /*for (int i = 0; i < SIZE * SIZE; i++)   {
             for (int j = 0; j < SIZE * SIZE ; j++) {
@@ -22,6 +22,48 @@ public class Q2_19 {
                     System.out.print(" ");
             }
             System.out.println();*/
+
+        printLine();
+        System.out.println();
+        printWindow();
+        printLine();
+        System.out.println();
+        printWindow();
+        printLine();
         }
+
+         static void printLine() {
+             System.out.print("+");
+             for (int i = 0; i < SIZE; i++) {
+                 System.out.print("=");
+             }
+             System.out.print("+");
+             for (int i = 0; i < SIZE; i++) {
+                 System.out.print("=");
+             }
+             System.out.print("+");
+
+         }
+         static void printWindow(){
+             for (int j = 0; j < SIZE; j++) {
+                 System.out.print("|");
+
+                 for (int i = 0; i < SIZE; i++) {
+                     System.out.print(" ");
+                 }
+
+                 System.out.print("|");
+                 for (int i = 0; i < SIZE; i++) {
+                     System.out.print(" ");
+                 }
+
+                 System.out.print("|");
+                 System.out.println();
+             }
+
+         }
+
+
+
     }
-}
+
